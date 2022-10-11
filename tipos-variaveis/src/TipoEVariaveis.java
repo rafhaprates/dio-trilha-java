7baa61e5c270d895cfc146eb6e20e4c4789c3be7
Rafhael Prates <rafhael.parra.ext@dasa.com.br>
2022-10-11 17:42:22 -0300

    mais commit

---
+++ tipos-variaveis/src/TipoEVariaveis.java
@@ -0,0 +1,20 @@
+public class TipoEVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 16050650;
+        long cpf = 44800267897L;
+
+
+        final String VALOR_DE_PI = "IRINEU VC NAO SABE NEM EU";
+
+        System.out.println(VALOR_DE_PI);
+
+
+
+
+
+    }
+}
